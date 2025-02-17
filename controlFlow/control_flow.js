9e0dccf7275bd4a6068e2463ff80616abd4c8382
Enrique Suica <enri1993@live.com>
2025-02-17 12:47:27 -0500

    Update control_flow.js

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,11 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
